6ce8966bccb3c2b676fc37e657df96000e6b7323
mid-kid <esteve.varela@gmail.com>
2020-05-03 15:38:42 +0200

    Remove useless code around wSeerOTNameGrammar

--- engine/events/poke_seer.asm
+++ engine/events/poke_seer.asm
@@ -255,24 +255,10 @@ GetCaughtOT:
 	ld de, wSeerOTName
 	ld bc, NAME_LENGTH
 	call CopyBytes
-
-; this routine is useless in Western localizations
-	ld hl, .male
-	ld a, [wSeerCaughtGender]
-	bit 7, a
-	jr z, .got_grammar
-	ld hl, .female
-
-.got_grammar
 	ld de, wSeerOTNameGrammar
 	ld a, "@"
 	ld [de], a
 	ret
-
-.male
-	db "@"
-.female
-	db "@"
 
 PrintSeerText:
 	ld e, a
